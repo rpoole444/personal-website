a3b535fad63490afca12ba3a89c157c4933cca1a
Reid Poole <poole.reid@gmail.com>
2024-03-06 12:10:33 -0700

    add nav and header to music page

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,11 +1,24 @@
+import Head from "next/head";
+import SocialMedia from "@/components/SocialMedia";
+import Navbar from "@/components/Navbar";
+
 const Music = () => {
   return (
-    <div className="container mx-auto px-4 text-center">
-      <h1 className="text-xl font-bold">Music Page</h1>
-      <p>Under Construction</p>
-      {/* ...Add your construction image here... */}
-    </div>
-  );
+    <>
+      <Head>
+        <title>About Me - Reid Poole</title>
+      </Head>
+      <SocialMedia />
+        <div className='flex row'>
+          <Navbar />
+          <div className="container mx-auto px-4 text-center">
+            <h1 className="text-xl font-bold">Music Page</h1>
+            <p>Under Construction</p>
+            {/* ...Add your construction image here... */}
+          </div>
+        </div>
+  </>
+    );
 };
 
 export default Music;
